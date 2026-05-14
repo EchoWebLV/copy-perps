adb3a513fdc222ee92b7f0beb6ada0a0bac2de8f
darkzer0isback <yordan.lasonov@gmail.com>
2026-05-14 16:56:11 +0200

    feat(bots): narrator knows all 6 headliner personas

--- lib/bots/narrator.ts
+++ lib/bots/narrator.ts
@@ -2,9 +2,19 @@
 import { generateText } from "ai";
 import { xai } from "@ai-sdk/xai";
 import { LIQUIDATION_LIZARD_PERSONA } from "./personas/liquidation-lizard";
+import { FUNDING_PHOEBE_PERSONA } from "./personas/funding-phoebe";
+import { MEAN_REVERT_MIKE_PERSONA } from "./personas/mean-revert-mike";
+import { MOMO_MAX_PERSONA } from "./personas/momo-max";
+import { VOL_VECTOR_PERSONA } from "./personas/vol-vector";
+import { BOOMER_TREND_PERSONA } from "./personas/boomer-trend";
 
 const PERSONAS = {
   "liquidation-lizard": LIQUIDATION_LIZARD_PERSONA,
+  "funding-phoebe": FUNDING_PHOEBE_PERSONA,
+  "mean-revert-mike": MEAN_REVERT_MIKE_PERSONA,
+  "momo-max": MOMO_MAX_PERSONA,
+  "vol-vector": VOL_VECTOR_PERSONA,
+  "boomer-trend": BOOMER_TREND_PERSONA,
 } as const;
 
 export type PersonaKey = keyof typeof PERSONAS;
